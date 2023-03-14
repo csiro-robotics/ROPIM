ce25e8e4974ae1e3bf0db29aca26a22e5ca77dd5
csiro_mohameds <csiro_mohameds@pmcc2-9-3.dug.com>
2023-03-14 09:38:26 +0800

    DUG jobscript update

--- simmim_pretraining_DUG.sh
+++ simmim_pretraining_DUG.sh
@@ -2,7 +2,7 @@
 #SBATCH -p csiro_od219033
 #SBATCH -C a100x4
 #SBATCH --time=24:00:00
-#SBATCH --nodes=4
+#SBATCH --nodes=2
 #SBATCH --cpus-per-task=8
 #SBATCH --ntasks-per-node=4
 #SBATCH --mail-user=sha457@csiro.au
@@ -14,10 +14,12 @@ echo "WORLD_SIZE="$WORLD_SIZE
 master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export MASTER_ADDR=$master_addr
 echo "MASTER_ADDR="$MASTER_ADDR
+export MASTER_PORT=$master_port
+
 . "/d/sw/miniconda3/4.8.3/etc/profile.d/conda.sh"
 conda activate /data/csiro_od219033/sw/venv/hs
-srun sh -c 'python -m torch.distributed.run main_simsim.py \
---world_size 16 --batch-size 190 --data-path /data/csiro_od219033/imagenet/train \
+srun sh -c 'python -m torch.distributed.launch --master_port 12346 --nproc_per_node 4 main_simsim.py \
+--world_size 8 --batch-size 380 --data-path /data/csiro_od219033/imagenet/train \
 --cfg configs/vit_base/simsim_pretrain__vit_base.yaml --Spatial_Sketching_Threshold -1 --sp 'True' \
 --eye_sp_sketch 'False' --tag simsim_pretrain_vit_base_imgnet_sp1SkR.07_LR2.5-5_LossDividedSum1_2_200LossMean_multinode_300e'
 # threshold 2 => .14
