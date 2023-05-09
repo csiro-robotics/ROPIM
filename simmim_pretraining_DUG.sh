f48947ae0461ea71c50a2d47a6a65ecdf3b910b3
Maryam Haghighat <maryam.haghighat88@gmail.com>
2023-05-09 10:22:40 +1000

    batch size 430

--- simmim_pretraining_DUG.sh
+++ simmim_pretraining_DUG.sh
@@ -13,7 +13,7 @@
 . "/d/sw/miniconda3/4.8.3/etc/profile.d/conda.sh"
 conda activate /data/csiro_od219033/sw/venv/hs
 srun sh -c 'python main_simsim.py \
---world_size 8 --batch-size 380 --data-path /data/csiro_od219033/imagenet/train \
+--world_size 8 --batch-size 430 --data-path /data/csiro_od219033/imagenet/train \
 --cfg configs/vit_base/simsim_pretrain__vit_base.yaml --Spatial_Sketching_Threshold 2 --sp 'True' \
 --eye_sp_sketch 'False' --tag simsim_pretrain_vit_base_imgnet_sp1SkR.14_LR5-5_LossDividedSum1_2_200LossMean_multinode_300e'
 # threshold 2 => .14
