d0b5f2eb8bfc87c4c6787abe4ade408eb5844976
Maryam Haghighat <maryam.haghighat88@gmail.com>
2023-05-15 22:02:48 +1000

    multi-step 100, 200, gamma.5

--- simmim_pretraining_DUG.sh
+++ simmim_pretraining_DUG.sh
@@ -15,6 +15,6 @@ conda activate /data/csiro_od219033/sw/venv/hs
 srun sh -c 'python main_simsim.py \
 --world_size 8 --batch-size 430 --data-path /data/csiro_od219033/imagenet/train \
 --cfg configs/vit_base/simsim_pretrain__vit_base.yaml --Spatial_Sketching_Threshold 2 --sp 'True' \
---eye_sp_sketch 'False' --tag simsim_pretrain_vit_base_imgnet_sp1SkR.14_LR5-5_LossDividedSum1_2_200LossMean_multinode_300e'
+--eye_sp_sketch 'False' --tag simsim_pretrain_vit_base_imgnet_sp1SkR.14_LR5-5_LossDividedSum1_2_200LossMean_multinode_300e_step_100_200_gamma.5'
 # threshold 2 => .14
 # threshold -1 => .07
