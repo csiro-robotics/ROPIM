c6929db88a95218147f9edd48673a9fb105def19
MaryamHaghighat <49633504+MaryamHaghighat@users.noreply.github.com>
2023-05-17 17:16:30 +1000

    Update utils.py

--- utils.py
+++ utils.py
@@ -29,7 +29,7 @@ def load_checkpoint(config, model, optimizer, lr_scheduler, logger):
     max_accuracy = 0.0
     if not config.EVAL_MODE and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
         optimizer.load_state_dict(checkpoint['optimizer'])
-#        lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
+        lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
         config.defrost()
         config.TRAIN.START_EPOCH = checkpoint['epoch'] + 1
         config.freeze()
